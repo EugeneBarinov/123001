9efc96cacbf190a33383ff3e3e3e22111e44af07
XmaynS <52330225+EugeneBarinov@users.noreply.github.com>
2025-08-22 14:27:00 +0300

    Delete features directory

--- features/__init__.py
+++
@@ -1,7 +0,0 @@
-from .indicators import compute_indicators, select_feature_matrix, DEFAULT_FEATURE_COLUMNS
-
-__all__ = [
-    "compute_indicators",
-    "select_feature_matrix",
-    "DEFAULT_FEATURE_COLUMNS",
-] 
